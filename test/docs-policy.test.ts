7cf9fd831ebf9de398fab462f62a85ac98325186
juan <47119916+jhomra21@users.noreply.github.com>
2026-09-19 15:12:45 -0500

    test: include repository map in writing policy

--- test/docs-policy.test.ts
+++ test/docs-policy.test.ts
@@ -3,6 +3,7 @@ import { readFile } from "node:fs/promises";
 
 const writingSources = [
   "README.md",
+  "docs/README.md",
   "docs/operations/RELEASING.md",
   "docs/operations/DEPLOYING.md",
   "CHANGELOG.md",
