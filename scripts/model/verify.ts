a97dfe17a68f0f1897f4a272dcd2756ebb4b932b
juan <47119916+jhomra21@users.noreply.github.com>
2026-09-20 10:16:42 -0500

    refactor: point model verifier at shared contracts

--- scripts/model/verify.ts
+++ scripts/model/verify.ts
@@ -5,7 +5,7 @@ import {
   MODEL_FILENAME,
   MODEL_SHA256,
   MODEL_SIZE_BYTES,
-} from "../../src/engine/model-config";
+} from "../../src/shared/model-config";
 import { inspectModelFile } from "../../src/shared/model-file";
 
 class ModelVerificationError extends Data.TaggedError("ModelVerificationError")<{
