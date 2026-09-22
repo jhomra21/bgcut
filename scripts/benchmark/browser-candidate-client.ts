c9f1aa02814599df90ab4816c10aee7a768e8359
juan <47119916+jhomra21@users.noreply.github.com>
2026-09-22 15:07:46 -0500

    style: separate diagnostic session statements

--- scripts/benchmark/browser-candidate-client.ts
+++ scripts/benchmark/browser-candidate-client.ts
@@ -649,6 +649,7 @@ const main = async (): Promise<void> => {
     );
 
     try {
+
       const diagnosticLogStart = diagnosticLogs.length;
 
       const diagnosticSession = await ort.InferenceSession.create(model, {
